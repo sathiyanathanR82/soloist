95fa15268fb411b53f49ad5dacf3dcdbd6c458e4
sathiyanathanR82 <sathiyanathan82@gmail.com>
2026-04-10 17:01:34 +0530

    feat: initialize environment configurations and production build index file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,20 +8,20 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:1000010000/api',
   apiUrl: 'https://soloist-backend-pxhl.onrender.com/api',
-  socialProviders: {
-    facebook: {
-      appId: 'YOUR_FACEBOOK_APP_ID',
-      version: 'v18.0'
-    },
-    google: {
-      clientId: 'YOUR_GOOGLE_CLIENT_ID'
-    },
-    microsoft: {
-      clientId: 'YOUR_MICROSOFT_CLIENT_ID',
-      authority: 'https://login.microsoftonline.com/common'
-    },
-    yahoo: {
-      clientId: 'YOUR_YAHOO_CLIENT_ID'
-    }
-  }
+  // socialProviders: {
+  //   facebook: {
+  //     appId: 'YOUR_FACEBOOK_APP_ID',
+  //     version: 'v18.0'
+  //   },
+  //   google: {
+  //     clientId: 'YOUR_GOOGLE_CLIENT_ID'
+  //   },
+  //   microsoft: {
+  //     clientId: 'YOUR_MICROSOFT_CLIENT_ID',
+  //     authority: 'https://login.microsoftonline.com/common'
+  //   },
+  //   yahoo: {
+  //     clientId: 'YOUR_YAHOO_CLIENT_ID'
+  //   }
+  // }
 };
